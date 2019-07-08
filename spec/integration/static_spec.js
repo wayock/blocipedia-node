479f71ac2422d53d20be64c38f020aa9a45d682d
Matthew Wayock <wayock@gmail.com>
2019-07-08 18:35:36 -0400

    scaffold project for delployment

---
+++ spec/integration/static_spec.js
@@ -0,0 +1,24 @@
+const request = require("request");
+const server = require("../../src/server");
+const base = "http://localhost:3000/";
+
+describe("routes : static", () => {
+
+//#1
+  describe("GET /", () => {
+
+//#2
+     it("should return status code 200 and have 'Welcome to Blocipedia' in the body of the response", (done) => {
+
+//#3
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toContain("Welcome to Blocipedia");
+
+//#4
+        done();
+      });
+    });
+
+  });
+});
---
+++ src/config/route-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  init(app){
+    const staticRoutes = require("../routes/static");
+
+    if(process.env.NODE_ENV === "test") {
+     const mockAuth = require("../../spec/support/mock-auth.js");
+     mockAuth.fakeIt(app);
+   }
+
+    app.use(staticRoutes);
+
+  }
+}
