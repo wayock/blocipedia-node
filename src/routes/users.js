eaedab1407c3470b9d650715ec611751d1411ad7
Matthew Wayock <wayock@gmail.com>
2019-07-12 09:03:23 -0400

    started signin work

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,10 +4,11 @@ const validation = require("./validation");
 const userController = require("../controllers/userController")
 
 router.get("/users/sign_up", userController.signUp);
-//router.get("/users/sign_in", userController.signInForm);
+router.get("/users/sign_in", userController.signInForm);
 //router.get("/users/sign_out", userController.signOut);
 //router.get("/users/:id", userController.show);
 router.post("/users", validation.validateUsers, userController.create);
+router.post("/users/sign_in", validation.validateUsers, userController.signIn);
 
 
 
