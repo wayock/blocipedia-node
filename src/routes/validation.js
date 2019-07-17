3b86a1acd55e12642eefbf0f053ddf44bcab321b
Matthew Wayock <wayock@gmail.com>
2019-07-16 21:19:11 -0400

    adjusting validations on wikis

--- src/routes/validation.js
+++ src/routes/validation.js
@@ -2,7 +2,7 @@ module.exports = {
 
   validateUsers(req, res, next) {
     if(req.method === "POST") {
-      req.checkBody("username", "must be at least 1 character in length").isLength({min: 1});
+      //req.checkBody("username", "must be at least 1 character in length").isLength({min: 1});
       req.checkBody("email", "must be valid").isEmail();
       req.checkBody("password", "must be at least 6 characters in length").isLength({min: 6});
       req.checkBody("passwordConfirmation", "must match password provided").optional().matches(req.body.password);
